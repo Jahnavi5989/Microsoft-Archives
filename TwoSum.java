1d8905716719cc07c7dae183a3b7785395cfaee7
Jahnavi.Bethapudi@KLEF <96164649+Jahnavi5989@users.noreply.github.com>
2022-08-29 08:35:55 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr=new int[2];
+       int l=0;
+        int r=n-1;
+        if(nums[l]+nums[r]==target){
+            return true;
+        }
+        else if(nums[i]+nums[r]>target){
+            r--;
+        }
+        else{
+            l++;
+        }
+        
+        return false;
+    }
+}
